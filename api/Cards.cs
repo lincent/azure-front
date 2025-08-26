a8ee8de569fca222faafcdfb1acf3389335ec6f7
lincent <11511477+lincent@users.noreply.github.com>
2025-08-26 17:09:50 +0100

    don't use bindings in static web apps for anything but http

--- api/Cards.cs
+++ api/Cards.cs
@@ -1,3 +1,5 @@
+using System.Text.Json;
+using Azure.Storage.Blobs;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
@@ -14,18 +16,33 @@ public record Card
 public class Cards
 {
     private readonly ILogger<Cards> _logger;
+    private readonly BlobServiceClient _blobServiceClient;
 
-    public Cards(ILogger<Cards> logger)
+    public Cards(ILogger<Cards> logger, BlobServiceClient blobServiceClient)
     {
         _logger = logger;
+        _blobServiceClient = blobServiceClient;
     }
 
     [Function("cards")]
-    public IActionResult Run(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
-        [BlobInput("test/cards.json", Connection = "BlobConnectionString")] Card[] cards
+    public async Task<IActionResult> Run(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req
     )
     {
+        var container = _blobServiceClient.GetBlobContainerClient("test");
+        var blob = container.GetBlobClient("cards.json");
+
+        if (!blob.Exists()) {
+            return new BadRequestObjectResult("Cards could not be found");
+        }
+
+        using var memoryStream = new MemoryStream();
+        await blob.DownloadToAsync(memoryStream);
+
+        memoryStream.Position = 0;
+
+        var cards = await JsonSerializer.DeserializeAsync<Card[]>(memoryStream);
+
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult(cards);
     }
